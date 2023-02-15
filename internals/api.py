4588a80bdf0b7e6546f55a417de17d49493ea99a
AXOME Studios <axomedev@gmail.com>
2023-02-15 16:19:38 +0100

    extended warning system

--- internals/api.py
+++ internals/api.py
@@ -1,16 +1,16 @@
 from ..helpers import load_json, write_json
-from ..constants import DATA_FILENAME
+from ..constants import DATA_FILENAME, SCREEN_PROCESS_NAME
 from .messageconstructor import construct_ban_message, construct_ban_message_chat, construct_kick_message, construct_kick_message_chat, construct_warning_message_chat
 from subprocess import Popen
 
-SKIP_COMMANDS = False # TODO: CHANGE before production to FALSE - otherwise, none of the actions will take effect.
+SKIP_COMMANDS = True # TODO: CHANGE before production to FALSE - otherwise, none of the actions will take effect.
 
 def type_into_server_console(command):
     if SKIP_COMMANDS: return
     Popen([
         "/usr/bin/screen",
         "-Rd",
-        "bedrock-server",
+        SCREEN_PROCESS_NAME,
         "-X",
         "stuff",
         "%s \r" % command
